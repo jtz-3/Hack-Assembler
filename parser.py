1975b8880164885993c586363c6cc86ab7c9cb45
jtz-3 <itzogana@uwaterloo.ca>
2025-10-29 17:17:53 -0400

    Added read_asm and next_inst functions

--- parser.py
+++ parser.py
@@ -8,14 +8,30 @@ https://www.nand2tetris.org/_files/ugd/44046b_7ef1c00a714c46768f08c459a6cab45a.p
 """
 import re
 
-# Necessary? just use file open method in main loop?
+# read_asm: Attempts to read in a given .asm file, throwing an error
+# if the file doesn't exist or is in the incorrect format
 def read_asm(fname: str):
-    return
+    if fname[-4:] is not '.asm':
+        print('Error: File is not a .asm file.')
+        return None
+    try:
+        return open(fname, 'r')
+    except FileNotFoundError:
+        print("Error: File '%s' not found." % fname)
+        return None
 
 # next_inst: Takes a file-like object representing a .asm script and 
-# returns the next instruction, or returns False if the end is reached.
+# returns the next instruction (stripped of whitespace), or returns None
+# if the line is a comment, or returns False if end of file is reached.
 def next_inst(file):
-    return
+    try:
+        new_line = next(file)
+        stripped_line = new_line.strip(' \n')
+        if stripped_line.startswith('//') or not stripped_line:
+            return None
+        return stripped_line
+    except StopIteration:
+        return False
 
 # inst_fields: Takes a string representing a well-written C- or A-instruction, and
 # returns a tuple containing the instruction type and field(s).
